beda2bf883ebbf3b8d89c3a9e0147aed5f26ae8f
Alifiandy Nugraha <alifiandyn@gmail.com>
2022-03-30 02:53:01 +0700

    Create API Documentation using Swagger UI

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const apiRouter = require("./server/routes");
+const errorHandler = require("./server/middlewares/errorHandler");
+const PORT = process.env.PORT || 4000;
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
+
+// api docs
+const swaggerUi = require("swagger-ui-express");
+// api docs configuration
+const swaggerDocument = require("./docs/swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
